9442964fd40dae1eb8717107474338b541cbef13
Evgeniy Surov <surovjeniya@gmail.com>
2023-03-16 12:25:42 +0300

    ...

--- app/src/update/personal.update.ts
+++ app/src/update/personal.update.ts
@@ -10,16 +10,16 @@ export class PersonalUpdate {
   @Command(Commands.personal)
   async personalCommand(@Ctx() ctx: TelegrafContext) {
     ctx.session.action = Actions['personal-specialist'];
-    await ctx.reply(
-      '✍️ Для создания индивидуального заказа отправьте техническое задание в нашу поддержку (https://t.me/nnaastyyaa)Если у вас нет технического задания, то можете так же написать нам (https://t.me/nnaastyyaa) и мы поможем его создать',
+    await ctx.replyWithHTML(
+      '✍️ Для создания индивидуального заказа отправьте техническое задание в нашу поддержку (https://t.me/sellershubanastasiya)Если у вас нет технического задания, то можете так же написать нам (https://t.me/nnaastyyaa) и мы поможем его создать',
     );
   }
 
   @Action(Commands.personal)
   async personalAction(@Ctx() ctx: TelegrafContext) {
     ctx.session.action = Actions['personal-specialist'];
-    await ctx.reply(
-      '✍️Для создания индивидуального заказа отправьте техническое задание в нашу поддержку (https://t.me/nnaastyyaa).\nЕсли у вас нет технического задания, то можете так же написать нам (https://t.me/nnaastyyaa) и мы поможем его создать',
+    await ctx.replyWithHTML(
+      '✍️Для создания индивидуального заказа отправьте техническое задание в нашу поддержку (https://t.me/sellershubanastasiya).\nЕсли у вас нет технического задания, то можете так же написать нам (https://t.me/nnaastyyaa) и мы поможем его создать',
     );
   }
 }
