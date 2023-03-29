75990569a42da463757bc96f2e5c40477ad9151f
surovjeniya <surovjeniya@gmail.com>
2023-03-29 14:32:35 +0300

    remove cors

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,14 +1,9 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { createProxyMiddleware } from 'http-proxy-middleware';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      allowedHeaders: '*',
-    },
-  });
+  const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
   await app.listen(configService.get<number>('PORT'));
