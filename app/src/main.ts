744906ca616a6a6d1096e0034e389fc70efe518b
surovjeniya <surovjeniya@gmail.com>
2023-03-29 12:38:10 +0300

    ...

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,9 +1,14 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      allowedHeaders: '*',
+    },
+  });
 
   const configService = app.get(ConfigService);
   await app.listen(configService.get<number>('PORT'));
