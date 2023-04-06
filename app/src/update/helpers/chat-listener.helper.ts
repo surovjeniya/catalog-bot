f3cd0a2947d84d55f1f4995b6c5e6ea7f43d36d4
surovjeniya <surovjeniya@gmail.com>
2023-04-06 16:48:03 +0300

    added conversation scenaries for spammers and sellers

--- app/src/update/helpers/chat-listener.helper.ts
+++ app/src/update/helpers/chat-listener.helper.ts
@@ -1,6 +1,7 @@
 import { ConfigService } from '@nestjs/config';
 import { TelegrafContext } from 'src/interface/telegraf.context';
 import { Utm } from 'src/user/user.entity';
+import { getSellerCase, getSpammerCase } from '../chat-cases';
 
 export const chatListener = async (
   ctx: TelegrafContext,
@@ -18,31 +19,96 @@ const managerChatListener = async (
   ctx: TelegrafContext,
   configService: ConfigService,
 ) => {
-  await ctx.replyWithHTML(
-    `Вы знали, что ваши сообщения удаляются спустя 5 минут?\nЗарегистрируйтесь в <a href="${configService.get(
-      'BOT_URL',
-    )}?start=${
-      Utm.chat_managers
-    }">нашем боте</a>, чтобы ваш пост висел вечность.\nХотите больше конверсии? Размещайте свою услугу в каталоге <a href="https://sellershub.ru">SellersHub</a> , где десятки тысячи селлеров ищут специалистов для сотрудничества.\nПо вопросам работы бота вы можете обратиться в нашу <a href="https://t.me/Nastyasellers">поддержку</a>.`,
-    {
+  const { text } = ctx.update.message;
+
+  if (getSpammerCase(text)) {
+    const messages = [
+      `Регистрируйтесь в <a href="${configService.get('BOT_URL')}?start=${
+        Utm.chat_managers
+      }">нашем боте</a>, чтобы размещать свои услуги или искать исполнителей.\nРегистрация в боте дает дополнительные бонусы как для специалистов, так и для селлеров:\n- доступ к отзывам и рейтингу\n-  подарки гайдов и чек-листов по работе на маркетплейсах.\nПо вопросам работы бота пишите в поддержку с пометкой “чат-бот”. (https://t.me/Nastyasellers)`,
+      `Используйте <a href="${configService.get('BOT_URL')}?start=${
+        Utm.chat_managers
+      }">наш бот</a> для поиска сотрудников или размещения своих услуг.\nРегистрация в боте дает доступ к гайдам и чек-листам по работе на маркетплейсах.\nПо вопросам работы бота пишите в поддержку с пометкой “чат-бот”. (https://t.me/Nastyasellers)`,
+    ];
+    await ctx.replyWithHTML(messages[Math.random().toFixed()], {
       reply_to_message_id: ctx.message.message_id,
-    },
-  );
+    });
+  }
+
+  if (getSellerCase(text)) {
+    await ctx.replyWithHTML(
+      `Спасибо, что ищите специалистов в нашем чате!\nПройдите быструю регистрацию в <a href="${configService.get(
+        'BOT_URL',
+      )}?start=${
+        Utm.chat_managers
+      }">нашем боте</a>, чтобы получать дополнительные бонусы и чек листы по  работе на маркетплейсе.\nПо вопросам работы бота пишите в поддержку с пометкой “чат-бот”. (https://t.me/Nastyasellers)`,
+      {
+        reply_to_message_id: ctx.message.message_id,
+      },
+    );
+  }
+
+  if (!getSellerCase(text) && !getSpammerCase(text)) {
+    await ctx.replyWithHTML(
+      `Вы знали, что ваши сообщения удаляются спустя 5 минут?\nЗарегистрируйтесь в <a href="${configService.get(
+        'BOT_URL',
+      )}?start=${
+        Utm.chat_managers
+      }">нашем боте</a>, чтобы ваш пост висел вечность.\nХотите больше конверсии? Размещайте свою услугу в каталоге <a href="${configService.get(
+        'WEB',
+      )}">SellersHub</a> , где десятки тысячи селлеров ищут специалистов для сотрудничества.\nПо вопросам работы бота вы можете обратиться в нашу <a href="https://t.me/Nastyasellers">поддержку</a>.`,
+      {
+        reply_to_message_id: ctx.message.message_id,
+      },
+    );
+  }
 };
 
 const infographicChatListener = async (
   ctx: TelegrafContext,
-
   configService: ConfigService,
 ) => {
-  await ctx.replyWithHTML(
-    `Вы знали, что ваши сообщения удаляются спустя 5 минут?\nЗарегистрируйтесь в <a href="${configService.get(
-      'BOT_URL',
-    )}?start=${
-      Utm.chat_design
-    }">нашем боте</a>, чтобы ваш пост висел вечность.\nХотите больше конверсии? Размещайте свою услугу в каталоге <a href="https://sellershub.ru">SellersHub</a> , где десятки тысячи селлеров ищут специалистов для сотрудничества.\nПо вопросам работы бота вы можете обратиться в нашу <a href="https://t.me/Nastyasellers">поддержку</a>.`,
-    {
+  const { text } = ctx.update.message;
+
+  if (getSpammerCase(text)) {
+    const messages = [
+      `Регистрируйтесь в <a href="${configService.get('BOT_URL')}?start=${
+        Utm.chat_design
+      }">нашем боте</a>, чтобы размещать свои услуги или искать исполнителей.\nРегистрация в боте дает дополнительные бонусы как для специалистов, так и для селлеров:\n- доступ к отзывам и рейтингу\n-  подарки гайдов и чек-листов по работе на маркетплейсах.\nПо вопросам работы бота пишите в поддержку с пометкой “чат-бот”. (https://t.me/Nastyasellers)`,
+      `Используйте <a href="${configService.get('BOT_URL')}?start=${
+        Utm.chat_design
+      }">наш бот</a> для поиска сотрудников или размещения своих услуг.\nРегистрация в боте дает доступ к гайдам и чек-листам по работе на маркетплейсах.\nПо вопросам работы бота пишите в поддержку с пометкой “чат-бот”. (https://t.me/Nastyasellers)`,
+    ];
+    await ctx.replyWithHTML(messages[Math.random().toFixed()], {
       reply_to_message_id: ctx.message.message_id,
-    },
-  );
+    });
+  }
+
+  if (getSellerCase(text)) {
+    await ctx.replyWithHTML(
+      `Спасибо, что ищите специалистов в нашем чате!\nПройдите быструю регистрацию в <a href="${configService.get(
+        'BOT_URL',
+      )}?start=${
+        Utm.chat_design
+      }">нашем боте</a>, чтобы получать дополнительные бонусы и чек листы по  работе на маркетплейсе.\nПо вопросам работы бота пишите в поддержку с пометкой “чат-бот”. (https://t.me/Nastyasellers)`,
+      {
+        reply_to_message_id: ctx.message.message_id,
+      },
+    );
+  }
+
+  if (!getSellerCase(text) && !getSpammerCase(text)) {
+    await ctx.replyWithHTML(
+      `Вы знали, что ваши сообщения удаляются спустя 5 минут?\nЗарегистрируйтесь в <a href="${configService.get(
+        'BOT_URL',
+      )}?start=${
+        Utm.chat_design
+      }">нашем боте</a>, чтобы ваш пост висел вечность.\nХотите больше конверсии? Размещайте свою услугу в каталоге <a href="${configService.get(
+        'WEB',
+      )}">SellersHub</a> , где десятки тысячи селлеров ищут специалистов для сотрудничества.\nПо вопросам работы бота вы можете обратиться в нашу <a href="https://t.me/Nastyasellers">поддержку</a>.`,
+      {
+        reply_to_message_id: ctx.message.message_id,
+      },
+    );
+  }
 };
