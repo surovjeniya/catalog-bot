1ba8d90177eb4141b59fb3b2a0150dd2ced4afe7
surovjeniya <surovjeniya@gmail.com>
2023-04-26 10:20:51 +0300

    ...

--- app/src/invite-user/invite-user.service.ts
+++ app/src/invite-user/invite-user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectBot } from 'nestjs-telegraf';
 import { TelegrafContext } from 'src/interface/telegraf.context';
+import { UserEntity } from 'src/user/user.entity';
 import { UserService } from 'src/user/user.service';
 import { Telegraf } from 'telegraf';
 import { BidFulFillmentDto } from './dto/bid-fulfillment.dto';
@@ -18,15 +19,23 @@ export class InviteUserService {
 
   async bidFulfillment({ contacts, service, service_id }: BidFulFillmentDto) {
     try {
-      const username = service.profile.contact_telegram
-        ? service.profile.contact_telegram.split('/')[1]
-        : null;
-      let userId: string = null;
+      // const username = service.profile.contact_telegram
+      //   ? service.profile.contact_telegram.split('/')[1]
+      //   : null;
+      // let userId: string = null;
+      // if (username) {
+      //   const user = await this.userService.findOne({ username });
+      //   if (user && user.telegram_id) {
+      //     userId = user.telegram_id;
+      //   }
+      // }
+      const username =
+        service.profile && service.profile.contact_telegram
+          ? service.profile.contact_telegram.split('/')[1]
+          : null;
+      let user: UserEntity = null;
       if (username) {
-        const user = await this.userService.findOne({ username });
-        if (user && user.telegram_id) {
-          userId = user.telegram_id;
-        }
+        user = await this.userService.findOne({ username });
       }
 
       const message = `
@@ -40,9 +49,9 @@ export class InviteUserService {
     Email: ${contacts.email ? contacts.email : 'отсутствует'}\n
     Id услуги: ${service.id}
     `;
-      // if (userId) {
-      //   await this.bot.telegram.sendMessage(userId, message);
-      // }
+      if (user) {
+        await this.bot.telegram.sendMessage(user.telegram_id, message);
+      }
       return await this.bot.telegram.sendMessage(54452505, message);
     } catch (e) {
       this.logger.error('Error from bidFulfillment', e.message);
