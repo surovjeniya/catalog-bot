0b4ada21cffe58ea57be7b5bd8d0d156825f504f
surovjeniya <surovjeniya@gmail.com>
2023-04-26 11:13:24 +0300

    ....

--- app/src/config/telegraf.config.ts
+++ app/src/config/telegraf.config.ts
@@ -2,19 +2,12 @@ import { ConfigService } from '@nestjs/config';
 import { TelegrafModuleOptions } from 'nestjs-telegraf';
 import * as LocalSession from 'telegraf-session-local';
 import * as path from 'path';
-import { join } from 'path';
-import { mkdir, existsSync } from 'fs';
-
-const sessionLib = join(__dirname, '..', '..', './src', './sessions');
-if (!existsSync(sessionLib)) {
-  mkdir(sessionLib, () => {});
-}
 
 export const sessions = new LocalSession({
   database:
     process.env.NODE_ENV === 'production'
-      ? path.join(sessionLib, 'sessions.json')
-      : path.join(sessionLib, 'sessions.development.json'),
+      ? path.join('sessions.json')
+      : path.join('sessions.development.json'),
 });
 
 export const getTelegrafConfig = (
